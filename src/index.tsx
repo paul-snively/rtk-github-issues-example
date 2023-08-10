e7574766645c376283ca74a48115baf353af8170
Paul Snively <psnively@mac.com>
2023-08-10 05:26:41 -0400

    Incremental progress—got rid of require. Now document ia undefined. Maybe I need a return, so the HTML is provided instead of the TypeScript being executed on the server?

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import { createRequire } from "https://deno.land/std@0.109.0/node/module.ts";
 const require = createRequire(import.meta.url);
 
-import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import App from './app/App.tsx'
 
 import store from './app/store.ts'
 
 <link rel="stylesheet" href="./index.css" />
 
 const render = () => {
-  const App = require('./app/App.tsx').default
-
   ReactDOM.render(
     <Provider store={store}>
       <App />
