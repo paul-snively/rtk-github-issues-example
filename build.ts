ebff123b830c3f307e674d54a6f8163072c1be78
Paul Snively <psnively@mac.com>
2023-08-14 13:16:35 -0400

    Forgot to revert build.ts to refer to external importMap.

--- build.ts
+++ build.ts
@@ -3,7 +3,7 @@ import { createBuilder } from "ultra/build.ts";
 const builder = createBuilder({
   browserEntrypoint: import.meta.resolve("./client.tsx"),
   serverEntrypoint: import.meta.resolve("./server.tsx"),
-  importMapPath: "./deno.json",
+  importMapPath: "./importMap.json",
 });
 
 builder.ignore([
