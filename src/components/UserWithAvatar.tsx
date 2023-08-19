da5ee8b43bfbe16868aed2ed8d1d6779bf0defad
Paul Snively <psnively@mac.com>
2023-08-19 10:31:20 -0400

    UserWithAvatar now appears to work. So the secret sauce seems to be combining react-helmet-async for adding a link element to the document head with the useAsset hook to refer to the right hashed asset.

--- src/components/UserWithAvatar.tsx
+++ src/components/UserWithAvatar.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import classnames from 'classnames'
+import { Helmet } from 'react-helmet-async';
 
 import { User } from '../api/githubAPI.tsx'
-
-<link rel="stylesheet" href="UserWithAvatar.module.css" />
+import useAsset from 'ultra/hooks/use-asset.js';
 
 interface UserAvatarProps {
   user: User
@@ -28,6 +28,9 @@ export const UserWithAvatar = ({
 
   const contents = (
     <React.Fragment>
+      <Helmet>
+        <link rel="stylesheet" href={useAsset("/UserWithAvatar.module.css")} />
+      </Helmet>
       <img className={avatarClassnames} src={user.avatar_url} alt="" />
       <div className={usernameClassnames}>{user.login}</div>
     </React.Fragment>
